52387ececf990c151a351397f1be3f7cb5bedebb
Jakub <jakub.tomczyk.2005@gmail.com>
2024-10-27 00:54:56 +0200

    basic working auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
+import { signIn } from "@/auth";
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1>Hello world</h1>
+        <div className="flex flex-col gap-2">
+          <form
+            action={async () => {
+              "use server";
+              await signIn("github");
+            }}
+          >
+            <button
+              type="submit"
+              className="bg-white text-black p-2 rounded-xl"
+            >
+              Signin with GitHub
+            </button>
+          </form>
+        </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         yapper.com
---
+++ src/app/protected/page.tsx
@@ -0,0 +1,23 @@
+import { auth, signOut } from "@/auth";
+
+export default async function Page() {
+  const session = await auth();
+
+  if (!session) {
+    return <div>Not authenticated</div>;
+  }
+
+  return (
+    <div className="container">
+      <pre>{JSON.stringify(session)}</pre>
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <button type="submit">Sign Out</button>
+      </form>
+    </div>
+  );
+}
